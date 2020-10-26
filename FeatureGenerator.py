8dac7167cc3e40559d0559cd599459620169f43d
Itay Ben Dan <itay@finzor.com>
2020-10-26 19:11:36 +0200

    modifications FeatureGenerator.py

--- FeatureGenerator.py
+++ FeatureGenerator.py
@@ -30,6 +30,8 @@ def create_covid_coutry_date_DataFrame(file_name,country_col = 'Country/Region',
     df = pd.read_csv(file_name)
     df["key"] = df.apply(lambda r: str(r[country_col])+("_"+str(r[province_col])).replace("_nan",""),axis=1)
     df = df.set_index("key")
+    df_mod = df[df.columns[start_col:]]
+    df_mod_t = df_mod.T
     df_mod_t = df_mod.T
     df_mod_t.index = df_mod_t.apply(lambda r: convert_dates(r.name),axis=1 )
     DF_countries = {c:pd.DataFrame(df_mod_t[c]) for c in df_mod_t.columns}
@@ -75,6 +77,7 @@ def create_df_dict(file_name,col_name,country_col='Country/Region',province_col=
     for c in DF_countries.keys():
         DF_countries[c] = DF_countries[c].rename(columns={c:col_name})
         if col_name.find("daily") == -1:
+            print("col_name is %s"%col_name)
             print(DF_countries[c][col_name].shape)
             print(DF_countries[c][col_name].tail())
             DF_countries[c]["daily_"+col_name] = DF_countries[c][col_name].diff(1).fillna(0)
