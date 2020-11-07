5630bf8ff80cf2e8181a787b08803b7cb204e291
Itay Ben Dan <itay@finzor.com>
2020-11-07 22:36:49 +0200

    fixing bugs key generate func from cols

--- CovidFeatureGenerator.py
+++ CovidFeatureGenerator.py
@@ -18,9 +18,11 @@ if __name__ == "__main__":
 		def lm(r):
 			s = str(r[cols[0]])
 			for c in cols[1:]:
-				s+= "_"+str(r[c]).replace("_nan","")
+				s+= ("_"+str(r[c])).replace("_nan","")
 			return s
 		return lm
+	key_cols2 = ['Country_Region','Province_State']
+
 
 	key_cols_func = c_func
 	start_main = time.time()
@@ -42,7 +44,7 @@ if __name__ == "__main__":
 	col_tar = "confirmed"
 
 	DF5,_ = main_generator(file_name=file_name,col_name=col_name,lags=lags,lags2=lags2,col_tar=col_tar,add=add,key_cols=key_cols,key_cols_func=key_cols_func)
-	exit(0)
+	
 	DF6 = DF5[[c for c in DF5.columns if not c in DF4.columns ]]
 
 	DFC = pd.concat([DF4,DF6],axis=1)
@@ -71,14 +73,14 @@ if __name__ == "__main__":
 
 
 
-	DF7,dict_indexes = main_generator(file_name=file_name,col_name=col_name,lags=lags,lags2=lags2,col_tar=col_tar,add = add_covid,country_col=country_col,province_col=province_col,dict_indexes=dict_indexes)
+	DF7,dict_indexes = main_generator(file_name=file_name,col_name=col_name,lags=lags,lags2=lags2,col_tar=col_tar,add = add_covid,key_cols=key_cols2,key_cols_func=key_cols_func,dict_indexes=dict_indexes)
 	file_name = orig_path+"csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
 	col_name = "confirmed"
 	lags = [1,7,14,28,56]
 	lags2 = [7,14,28]
 	col_tar = "confirmed"
 
-	DF8,_ = main_generator(file_name=file_name,col_name=col_name,lags=lags,lags2=lags2,col_tar=col_tar,add= add_covid,country_col=country_col,province_col=province_col,dict_indexes=dict_indexes)
+	DF8,_ = main_generator(file_name=file_name,col_name=col_name,lags=lags,lags2=lags2,col_tar=col_tar,add= add_covid,key_cols=key_cols2,key_cols_func=key_cols_func,dict_indexes=dict_indexes)
 
 	DF9 = DF8[[c for c in DF8.columns if not c in DF7.columns ]]
 
@@ -131,5 +133,5 @@ if __name__ == "__main__":
 		a_c =feature_mixing(gfn[k], prefix1,prefix2, filter_strings1,filter_strings2,operators)
 	print("calc features time is %0.2f"%(time.time()-start_t))
 	from_gf_to_df(dfn,gfn,a_c)
-	dfn.to_csv(os.path.join(output_path,"test_feature_mixing_covid19_wUS.csv"),index_label="index")
+	dfn.to_csv(os.path.join(output_path,"test_feature_mixing_covid19_wUS2.csv"),index_label="index")
 	print("TOTAL including feature mixing %0.2f"%(time.time() - start_main))
